869631109cac624a2124a55d1649d91fcf246b0e
jpalencia77 <jpalencia@ucol.mx>
2023-06-29 21:03:21 -0500

    favourites and favourites bar added

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ import { Navigation } from "./src/infrastructure/navigation/";
 
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
-
+import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 export default function App() {
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
@@ -32,11 +32,15 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <LocationContextProvider>
-          <RestaurantsContextProvider>
-            <Navigation />
-          </RestaurantsContextProvider>
-        </LocationContextProvider>
+        <FavouritesContextProvider>
+
+
+          <LocationContextProvider>
+            <RestaurantsContextProvider>
+              <Navigation />
+            </RestaurantsContextProvider>
+          </LocationContextProvider>
+        </FavouritesContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
